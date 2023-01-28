92df9585b16d9e724fbd5b49e5fa40871aca1a02
Mateus da Silva <mso3@cin.ufpe.br>
2023-01-28 15:32:03 -0300

    Adicionado a funcionalidade do relógio analógico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,33 @@
 let digital = document.querySelector('.digital');
-let pointerSeconds = document.querySelector('.ponteiros p p_s');
-let pointerMinutes = document.querySelector('.poteiros p p_m');
-let pointerHours = document.querySelector('.ponteiros p p_h');
+let pointerSeconds = document.querySelector('.p_s');
+let pointerMinutes = document.querySelector('.p_m');
+let pointerHours = document.querySelector('.p_h');
 
 const fixZero = (time)=>{
     if(time<10){
-        return `0${time}`
+        return `0${time}`;
     }else{
-       return time
+       return time;
     }
 }
 
 const updateClock =()=>{
     var date = new Date();
-    var hours = date.getHours()
-    var minutes = date.getMinutes()
-    var seconds = date.getSeconds()
-    digital.innerText = `${fixZero(hours)}:${fixZero(minutes)}:${fixZero(seconds)}`
-}
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    digital.innerText = `${fixZero(hours)}:${fixZero(minutes)}:${fixZero(seconds)}`;
 
+    let secondsDeg = (360/60)*seconds -90;
+    let minutesDeg = (360/60)*minutes -90;
+    let hoursDeg = (360/12)*hours -90;
+
+    pointerSeconds.style.transform = `rotate(${secondsDeg}deg)`;
+    pointerMinutes.style.transform = `rotate(${minutesDeg}deg)`;
+    pointerHours.style.transform = `rotate(${hoursDeg}deg)`;
+
+
+
+}
+updateClock();
 setInterval(updateClock, 1000)
